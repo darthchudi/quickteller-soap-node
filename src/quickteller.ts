44a14119d946cf148b6f638e75b30231293c1d54
darthchudi <chukwudioranu@ymail.com>
2019-05-12 23:27:19 +0100

    feat: refactor class & add send bill payment advice method

--- src/quickteller.ts
+++ src/quickteller.ts
@@ -1,10 +1,13 @@
 import soap, { Client } from 'soap';
-import { promisify } from 'util';
-import { Parser, Builder } from 'xml2js';
-import _ from 'lodash';
 import env from './env';
 import QuicktellerError from './errors';
 import { QUICKTELLER_SUCCESS } from './constants';
+import {
+  toJSON,
+  removeEmptyFields,
+  buildArguments,
+  generateReference,
+} from './utils';
 
 /**
  * Base quickteller class
@@ -16,38 +19,22 @@ class Quickteller {
   private client: Client;
 
   /**
-   * Converts an XML string to a javascript object.
-   */
-  private toJSON: <T = any>(xml: string) => Promise<T>;
-
-  /**
-   * Converts a javascript object to an XML string
-   */
-  private toXML: (obj: object) => string;
-
-  /**
    * Creates the quickteller SOAP client and the JSON & SOAP parsers for serializing and deserializng arguments and responses.
+   * @param requestPrefix 4 digit request prefix provided by Interswitch
    */
   async init() {
-    if (!env.quickteller_soap_url || !env.quickteller_terminal_id)
+    if (
+      !env.quickteller_soap_url ||
+      !env.quickteller_terminal_id ||
+      !env.quickteller_request_prefix
+    )
       throw new Error(
-        'The following environment variables are required to initialize the Quickteller client: QUICKTELLER_SOAP_URL, QUICKTELLER_TERMINAL_ID'
+        `The following environment variables are required to initialize the Quickteller client: QUICKTELLER_SOAP_URL, 
+        QUICKTELLER_TERMINAL_ID, QUICKTELLER_REQUEST_PREFIX`
       );
 
     // TODO: Review the implications of doing this
     process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
-
-    const xmlParser = new Parser({ explicitArray: false, mergeAttrs: true });
-
-    const jsonParser = new Builder({
-      headless: true,
-      renderOpts: {
-        indent: '',
-      },
-    });
-
-    this.toJSON = promisify(xmlParser.parseString);
-    this.toXML = jsonParser.buildObject.bind(jsonParser);
 
     this.client = await soap.createClientAsync(
       `${env.quickteller_soap_url}?wsdl`,
@@ -66,18 +53,6 @@ class Quickteller {
   }
 
   /**
-   * Converts a javascript object `args` to an XML string and returns a JSON object that the `node-soap` can use to make a SOAP request that
-   * Quickteller recognizes.
-   * @param args The request arguments to be serialized to XML
-   */
-  private argumentBuilder(args: object) {
-    const xml = this.toXML(args);
-    return {
-      xmlParams: `<![CDATA[${xml}]]>`,
-    };
-  }
-
-  /**
    * Gets all the Quickteller methods available in the SOAP service.
    */
   getMethods() {
@@ -85,16 +60,16 @@ class Quickteller {
   }
 
   /**
-   * Gets all billter categories.
+   * Gets all biller categories.
    */
   async getBillerCategories() {
     this.isClientInitialized();
     //@ts-ignore
     const [rawResult] = await this.client.GetBillerCategoriesAsync();
-    const { Response } = await this.toJSON(rawResult.GetBillerCategoriesResult);
+    const { Response } = await toJSON(rawResult.GetBillerCategoriesResult);
 
     if (Response.ResponseCode === QUICKTELLER_SUCCESS)
-      return Response.CategoryList;
+      return Response.CategoryList.Category;
 
     throw new QuicktellerError(
       'An error occured while getting biller categories',
@@ -118,55 +93,28 @@ class Quickteller {
   ) {
     this.isClientInitialized();
 
-    const _searchCriteria = {
+    // Filter out undefined keys/props
+    const SearchCriteria = removeEmptyFields({
       CategoryId,
       BillerId,
       ChannelId,
       BillerName,
       TerminalId: env.quickteller_terminal_id,
-    };
+    });
 
-    // Filter out undefined keys/props
-    const SearchCriteria = _.pickBy(_searchCriteria, v => v !== undefined);
-
-    const args = this.argumentBuilder({
+    const args = buildArguments({
       SearchCriteria,
     });
 
     //@ts-ignore
     const [rawResult] = await this.client.GetBillersAsync(args);
-    const { Response } = await this.toJSON(rawResult.GetBillersResult);
+    const { Response } = await toJSON(rawResult.GetBillersResult);
 
     if (Response.ResponseCode === QUICKTELLER_SUCCESS)
-      return Response.BillerList;
+      return Response.BillerList.Category.Biller;
 
     throw new QuicktellerError(
       'An error occured while getting billers',
-      Response.ResponseCode,
-      Response.ResponseDescription
-    );
-  }
-
-  /**
-   * Gets all of a biller's payment items
-   * @param BillerId The biller's id
-   */
-  async getBillerPaymentItems(BillerId: number | string) {
-    this.isClientInitialized();
-
-    const args = this.argumentBuilder({ SearchCriteria: { BillerId } });
-
-    //@ts-ignore
-    const [rawResult] = await this.client.GetBillerPaymentItemsAsync(args);
-    const { Response } = await this.toJSON(
-      rawResult.GetBillerPaymentItemsResult
-    );
-
-    if (Response.ResponseCode === QUICKTELLER_SUCCESS)
-      return Response.PaymentItemList.PaymentItem;
-
-    throw new QuicktellerError(
-      'An error occured while getting biller payment items',
       Response.ResponseCode,
       Response.ResponseDescription
     );
@@ -180,13 +128,36 @@ class Quickteller {
 
     //@ts-ignore
     const [rawResult] = await this.client.GetLatestBillersAsync();
-    const { Response } = await this.toJSON(rawResult.GetLatestBillersResult);
+    const { Response } = await toJSON(rawResult.GetLatestBillersResult);
 
     if (Response.ResponseCode === QUICKTELLER_SUCCESS)
       return Response.BillerList;
 
     throw new QuicktellerError(
       'An error occured while getting latest billers',
+      Response.ResponseCode,
+      Response.ResponseDescription
+    );
+  }
+
+  /**
+   * Gets all of a biller's payment items
+   * @param BillerId The biller's id
+   */
+  async getBillerPaymentItems(BillerId: number | string) {
+    this.isClientInitialized();
+
+    const args = buildArguments({ SearchCriteria: { BillerId } });
+
+    //@ts-ignore
+    const [rawResult] = await this.client.GetBillerPaymentItemsAsync(args);
+    const { Response } = await toJSON(rawResult.GetBillerPaymentItemsResult);
+
+    if (Response.ResponseCode === QUICKTELLER_SUCCESS)
+      return Response.PaymentItemList.PaymentItem;
+
+    throw new QuicktellerError(
+      'An error occured while getting biller payment items',
       Response.ResponseCode,
       Response.ResponseDescription
     );
@@ -203,30 +174,29 @@ class Quickteller {
   async validateCustomer(
     PaymentCode: string | number,
     CustomerId: string | number,
-    CustomerValidationField: string,
+    CustomerValidationField?: string,
     WithDetails = true
   ) {
     this.isClientInitialized();
 
-    const _args = {
-      RequestDetails: {
-        TerminalId: env.quickteller_terminal_id,
-        Customer: [
-          {
-            PaymentCode,
-            CustomerId,
-            CustomerValidationField,
-            WithDetails: WithDetails ? 'True' : 'False',
-          },
-        ],
-      },
+    // Filter out undefined keys/props
+    const customerParams = removeEmptyFields({
+      PaymentCode,
+      CustomerId,
+      CustomerValidationField,
+      WithDetails: WithDetails ? 'True' : 'False',
+    });
+
+    const RequestDetails = {
+      TerminalId: env.quickteller_terminal_id,
+      Customer: customerParams,
     };
 
-    const args = this.argumentBuilder(_args);
+    const args = buildArguments({ RequestDetails });
 
     //@ts-ignore
     const [rawResult] = await this.client.ValidateCustomerAsync(args);
-    const { Response } = await this.toJSON(rawResult.ValidateCustomerResult);
+    const { Response } = await toJSON(rawResult.ValidateCustomerResult);
 
     const { ResponseCode, ResponseDescription, Customer } = Response;
 
@@ -239,12 +209,54 @@ class Quickteller {
 
     if (Customer.ResponseCode !== QUICKTELLER_SUCCESS)
       throw new QuicktellerError(
-        `An error occured while validating ${CustomerValidationField} ${CustomerId}`,
+        `An error occured while validating ${
+          CustomerValidationField ? CustomerValidationField : 'customer id'
+        } ${CustomerId}`,
         Customer.ResponseCode,
         Customer.ResponseDescription
       );
 
     return Customer;
+  }
+
+  /**
+   * Notifies the biller of the payment
+   */
+  async sendBillPaymentAdvice(
+    Amount: number,
+    PaymentCode: string | number,
+    CustomerId: string | number,
+    CustomerMobile?: string,
+    CustomerEmail?: string
+  ) {
+    this.isClientInitialized();
+
+    const reference = await generateReference(8);
+
+    // Filter out undefined keys/props
+    const BillPaymentAdvice = removeEmptyFields({
+      Amount,
+      PaymentCode,
+      CustomerId,
+      CustomerMobile,
+      CustomerEmail,
+      TerminalId: env.quickteller_terminal_id,
+      RequestReference: `${env.quickteller_request_prefix}${reference}`,
+    });
+
+    const args = buildArguments({ BillPaymentAdvice });
+
+    //@ts-ignore
+    const [rawResult] = await this.client.SendBillPaymentAdviceAsync(args);
+    const { Response } = await toJSON(rawResult.SendBillPaymentAdviceResult);
+
+    if (Response.ResponseCode === QUICKTELLER_SUCCESS) return Response;
+
+    throw new QuicktellerError(
+      'An error occured while sending bill payment advice',
+      Response.ResponseCode,
+      Response.ResponseDescription
+    );
   }
 }
 
