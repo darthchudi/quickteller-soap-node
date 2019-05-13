ca872eb504383fd65e2d46efc6c4fee3bd5e837b
darthchudi <chukwudioranu@ymail.com>
2019-05-13 12:52:10 +0100

    feat: pass type arguments to lodash pickBy function

--- src/utils/removeEmptyFields.ts
+++ src/utils/removeEmptyFields.ts
@@ -1,8 +1,9 @@
 import { pickBy } from 'lodash';
+import { Object } from '../typings';
 
 /**
  * Removes null and undefined fields from an object
  * @param obj The object to be stripped of empty fields
  */
 export const removeEmptyFields = (obj: object) =>
-  pickBy(obj, v => v !== undefined && v !== null);
+  pickBy<Object>(obj, v => v !== undefined && v !== null);
