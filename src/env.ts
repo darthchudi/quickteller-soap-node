12574c37e16bc46ab89926f9de4fb4ea2b22a0f6
darthchudi <chukwudioranu@ymail.com>
2019-05-14 22:41:40 +0100

    chore: remove env file

--- src/env.ts
+++
@@ -1,9 +0,0 @@
-import dotenv from 'dotenv';
-
-dotenv.config();
-
-export default {
-  quickteller_soap_url: process.env.QUICKTELLER_SOAP_URL,
-  quickteller_terminal_id: process.env.QUICKTELLER_TERMINAL_ID,
-  quickteller_request_prefix: process.env.QUICKTELLER_REQUEST_PREFIX,
-};
