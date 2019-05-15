56128a5ca408fd2336221d334ea322c25351e19f
darthchudi <chukwudioranu@ymail.com>
2019-05-15 08:59:59 +0100

    feat: tests for payment advice and transaction query

--- src/tests/quickteller.spec.ts
+++ src/tests/quickteller.spec.ts
@@ -1,6 +1,13 @@
-import quickteller from '../';
 import dotenv from 'dotenv';
-import { QuicktellerError } from '../errors';
+import quickteller, {
+  QuicktellerError,
+  QUICKTELLER_BILLER_NOT_FOUND,
+  QUICKTELLER_INVALID_TRANSACTION_AMOUNT,
+  QUICKTELLER_UNRECOGNIZED_CUSTOMER,
+  QUICKTELLER_XML_NODE_EMPTY,
+  QUICKTELLER_XML_NODE_MISSING,
+} from '../';
+import { QUICKTELLER_DATA_NOT_FOUND } from '../constants';
 
 beforeAll(async () => {
   dotenv.config();
@@ -68,7 +75,7 @@ test('it throws an error if Quickteller responds with an empty list for a biller
   } catch (err) {
     expect(err).toBeInstanceOf(QuicktellerError);
     expect(err.message).toBe(
-      'Quickteller could not find billers that satsify this query'
+      'Quickteller could not find billers that satisfy this query'
     );
   }
 });
@@ -99,13 +106,14 @@ test("it gets a biller's payment items", async () => {
 });
 
 test("it throws an error if Quickteller is unable to find a biller's payment items", async () => {
-  expect.assertions(2);
+  expect.assertions(3);
 
   try {
     await quickteller.getBillerPaymentItems(4949494);
   } catch (err) {
     expect(err).toBeInstanceOf(QuicktellerError);
     expect(err.message).toMatch(/Quickteller could not find the biller/);
+    expect(err.quickteller_code).toBe(QUICKTELLER_BILLER_NOT_FOUND);
   }
 });
 
@@ -121,7 +129,7 @@ test('it validates a customer', async () => {
 });
 
 test('it throws an error if a customer fails a validation', async () => {
-  expect.assertions(2);
+  expect.assertions(3);
 
   try {
     await quickteller.validateCustomer(10401, 'dmefrfmkrf');
@@ -130,6 +138,7 @@ test('it throws an error if a customer fails a validation', async () => {
     expect(err.message).toMatch(
       /An error occured while validating customer id/
     );
+    expect(err.quickteller_code).toBe(QUICKTELLER_UNRECOGNIZED_CUSTOMER);
   }
 });
 
@@ -143,5 +152,130 @@ test('it throws an error if an invalid payment code is provided for customer val
     expect(err.message).toMatch(
       /PaymentTypeValidation Process should have run before calling this property/
     );
+  }
+});
+
+test('it sends a payment advice', async () => {
+  const billPaymentAdvice = await quickteller.sendBillPaymentAdvice(
+    1460000,
+    10401,
+    '0000000001',
+    '08073292354',
+    'frankdonga@ymail.com'
+  );
+
+  expect(billPaymentAdvice.ApprovedAmount).toBe('1460000');
+  expect(billPaymentAdvice.RequestReference).toBeDefined();
+  expect(billPaymentAdvice.TransactionRef).toBeDefined();
+  expect(billPaymentAdvice.ResponseDescription).toBe('Success');
+});
+
+test('it throws an error if a payment advice is sent with an invalid amount', async () => {
+  expect.assertions(4);
+
+  try {
+    await quickteller.sendBillPaymentAdvice(460000, 10401, '0000000001');
+  } catch (err) {
+    expect(err).toBeInstanceOf(QuicktellerError);
+    expect(err.message).toMatch(
+      /An error occured while sending bill payment advice/
+    );
+    expect(err.description).toMatch(
+      /Transaction amount not approved by biller/
+    );
+    expect(err.quickteller_code).toBe(QUICKTELLER_INVALID_TRANSACTION_AMOUNT);
+  }
+});
+
+test('it throws an error if a payment advice is sent with a missing amount', async () => {
+  expect.assertions(3);
+
+  try {
+    await quickteller.sendBillPaymentAdvice(undefined, 10401, '0000000001');
+  } catch (err) {
+    expect(err).toBeInstanceOf(QuicktellerError);
+    expect(err.message).toMatch(
+      /An error occured while sending bill payment advice/
+    );
+    expect(err.quickteller_code).toBe(QUICKTELLER_XML_NODE_MISSING);
+  }
+});
+
+test('it throws an error if a payment advice is sent with an invalid payment code', async () => {
+  expect.assertions(3);
+
+  try {
+    await quickteller.sendBillPaymentAdvice(1460000, 84848, '0000000001');
+  } catch (err) {
+    expect(err).toBeInstanceOf(QuicktellerError);
+    expect(err.message).toMatch(/Quickteller could not find the biller/);
+    expect(err.quickteller_code).toBe(QUICKTELLER_BILLER_NOT_FOUND);
+  }
+});
+
+test('it throws an error if a payment advice is sent with a missing payment code', async () => {
+  expect.assertions(3);
+
+  try {
+    await quickteller.sendBillPaymentAdvice(1460000, undefined, '0000000001');
+  } catch (err) {
+    expect(err).toBeInstanceOf(QuicktellerError);
+    expect(err.message).toMatch(
+      /An error occured while sending bill payment advice/
+    );
+    expect(err.quickteller_code).toBe(QUICKTELLER_XML_NODE_EMPTY);
+  }
+});
+
+test('it throws an error if a payment advice is sent with an invalid customer id', async () => {
+  expect.assertions(3);
+
+  try {
+    await quickteller.sendBillPaymentAdvice(1460000, 10401, 393939);
+  } catch (err) {
+    expect(err).toBeInstanceOf(QuicktellerError);
+    expect(err.message).toMatch(
+      /An error occured while sending bill payment advice/
+    );
+    expect(err.quickteller_code).toBe(QUICKTELLER_UNRECOGNIZED_CUSTOMER);
+  }
+});
+
+test('it throws an error if a payment advice is sent with a missing customer id', async () => {
+  expect.assertions(3);
+
+  try {
+    await quickteller.sendBillPaymentAdvice(1460000, 10401, undefined);
+  } catch (err) {
+    expect(err).toBeInstanceOf(QuicktellerError);
+    expect(err.message).toMatch(
+      /An error occured while sending bill payment advice/
+    );
+    expect(err.quickteller_code).toBe(QUICKTELLER_XML_NODE_EMPTY);
+  }
+});
+
+test('it queries a transaction', async () => {
+  const transactionQuery = await quickteller.queryTransaction('141344a13b06');
+  expect(transactionQuery.ServiceName).toBe('Premium');
+  expect(transactionQuery.Status).toBe('Complete');
+  expect(transactionQuery.RequestReference).toBe('141344a13b06');
+  expect(transactionQuery.TransactionSet).toBe('BillPayment');
+  expect(transactionQuery.CustomerMobile).toBe('08073292354');
+  expect(transactionQuery.CustomerEmail).toBe('frankdonga@ymail.com');
+  expect(transactionQuery.BillPayment.BillerId).toBe('104');
+  expect(transactionQuery.BillPayment.CustomerId1).toBe('0000000001');
+});
+
+test('it throws an error if an unknown transaction is queried', async () => {
+  expect.assertions(4);
+
+  try {
+    await quickteller.queryTransaction('ejfrfnrjfnjrnf');
+  } catch (err) {
+    expect(err).toBeInstanceOf(QuicktellerError);
+    expect(err.message).toMatch(/An error occured while querying transaction/);
+    expect(err.quickteller_code).toBe(QUICKTELLER_DATA_NOT_FOUND);
+    expect(err.description).toBe('Transaction not found');
   }
 });
